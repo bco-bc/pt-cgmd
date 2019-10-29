6253bca16f5c8f0c33d8f1f89785a67a9cdc2746
André H. Juffer <andre.juffer@triacle-bc.com>
2019-10-29 10:31:28 +0200

    netbeans

--- simulation/src/sim-util.cpp
+++ simulation/src/sim-util.cpp
@@ -38,11 +38,13 @@ namespace simploce {
         
         length_t cutoffDistance(const box_ptr_t& box)
         {
-            //return 0.5 * box->size();            
+            return 0.5 * box->size();            
+            /*
             length_t halve = 0.5 * box->size();
             return conf::RCUTOFF_DISTANCE_() > halve() ? 
                    halve : 
                    conf::RCUTOFF_DISTANCE_;             
+             */
         }
         
         real_t squareCutoffDistance(const box_ptr_t& box)
