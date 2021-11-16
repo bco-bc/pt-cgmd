3e51338271ceca7b01abb5dfc44df0af27145abc
André H Juffer <andre.juffer@triacle-bc.com>
2021-11-16 09:47:18 +0200

    backup2

---
+++ util/cmake-build-release/tests/CMakeFiles/param-test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/param-test.dir/param-test.cpp.o"
+  "param-test"
+  "param-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/param-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
